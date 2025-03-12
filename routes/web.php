02984818144c1cdb3d78236c2869eef0f9619279
mikaanh <elsadanapitupulu26@gmail.com>
2025-03-12 14:52:09 +0700

    perbaruan kode ListBarangController, list_barang.blade dan web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListBarangController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+  Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
+     return view('list_barang', compact('id', 'nama')); 
+    });
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
